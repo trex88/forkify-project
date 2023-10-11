9ccbd8ffa3a4b2d9f3ca5ae61860ee9723572457
trex88 <strawhatluffy199990@gmail.com>
2023-10-11 17:40:56 +1100

    Change outdated Fraction library to new one

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,7 @@
 import View from './view.js';
 import icons from 'url:../../img/icons.svg';
 // import { Fraction } from 'fractional';
+import Fraction from 'fraction.js';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -129,7 +130,7 @@ class RecipeView extends View {
               <use href="${icons}#icon-check"></use>
             </svg>
             <div class="recipe__quantity">${
-              ing.quantity ? ing.quantity : ''
+              ing.quantity ? new Fraction(ing.quantity).toFraction(true) : ''
             }</div>
             <div class="recipe__description">
               <span class="recipe__unit">${ing.unit}</span>
