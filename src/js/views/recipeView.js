253c22ea603c22432e43c7d6b41c82bd991afd0c
trex88 <strawhatluffy199990@gmail.com>
2023-10-11 17:44:59 +1100

    Updated master

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,5 @@
 import View from './view.js';
 import icons from 'url:../../img/icons.svg';
-// import { Fraction } from 'fractional';
 import Fraction from 'fraction.js';
 
 class RecipeView extends View {
